21995f3f45990844bc9f4b270eeade3d94d48d69
Mark Woodward <markwood117@gmail.com>
2018-10-30 14:43:21 -0700

    initial commit; basic CRUD added

---
+++ js/scripts.js
@@ -0,0 +1,134 @@
+var taskList = [];
+var defaultDate  = new Date();
+var defaultYear  = defaultDate.toString().split(' ')[3];
+var defaultMonth = defaultDate.getMonth();
+var defaultDay   = defaultDate.toString().split(' ')[2];
+
+$(document).ready(function(){
+
+  var validText = function (arg) {
+    return ((typeof arg === "string")&&((arg.trim()).length>0));
+  };
+  var flashErrors = function() {
+    if ( $("#ERROR-MSG").children().length > 0) {
+      $("#ERROR-MSG").fadeIn(2000);
+      setTimeout(function() {
+        $("#ERROR-MSG>*").fadeOut(1000);
+        $("#ERROR-MSG").fadeOut(1500);
+        $("#ERROR-MSG").empty();
+      }, 4000);
+    }
+  };
+
+  Task = function(newName,newDate) {
+    this.name=newName;
+    this.deadline=newDate;
+
+    taskList.push(this);
+    this.index=(taskList.length-1);
+  };
+
+  Task.prototype.removeTask = function () {
+    taskList.pop(this.index);
+  };
+  Task.prototype.editTask = function (newName,newDate) {
+    this.name=newName;
+    this.deadline=newDate;
+  };
+
+  //LIST
+  var listTasks = function(){
+    $("#LIST").empty();
+    taskList.forEach(function(i,e){
+      i.index = e;
+      $("#LIST").append("<li>" + i.index + " : " + i.name + " -- (" + i.deadline + ")</li>");
+    });
+  };
+
+  $("#LEFT-DISPLAY>div,#ERROR-MSG").hide();
+  $("#OPTIONS").show();
+  //ADD
+  $("button[name='addButton']").click(function(){
+    $("input[name='new-deadLine']").val(defaultYear+"-"+(defaultMonth+1).toString()+"-"+defaultDay);
+    $("#INPUT-newTask").toggle();
+    $("#OPTIONS").toggle();
+  });
+
+  $("#INPUT-newTask>form").submit(function(event){
+    event.preventDefault();
+    var name = $("input[name='new-name']").val();
+    var deadLine = $("input[name='new-deadLine']").val();
+
+    if ( (validText(name)) && (validText(deadLine)) ) {
+      new Task(name,deadLine);
+    }
+    else if( !(validText(name)) ){ $("#ERROR-MSG").append("<li>Error: *NAME* field was either left empty or invalid</li>"); }
+    else if( !(validText(deadLine)) ) { $("#ERROR-MSG").append("<li>Error: *DEADLINE* field was either left empty or invalid</li>"); }
+
+    flashErrors();
+    listTasks();
+  });
+
+  //EDIT
+  $("button[name=editButton]").click(function(){
+    $("input[name='edit-index']").val("0");
+    $("input[name='edit-deadLine']").val(defaultYear+"-"+(defaultMonth+1).toString()+"-"+defaultDay);
+    $("#INPUT-editTask").toggle();
+    $("#OPTIONS").toggle();
+  });
+
+  $("#INPUT-editTask>form").submit(function(event){
+    event.preventDefault();
+    var index    = parseInt( $("input[name='edit-index']").val() );
+    var name     = $("input[name='edit-name']").val();
+    var deadLine = $("input[name='edit-deadLine']").val();
+
+    console.log(deadLine+" : "+typeof deadLine);
+
+    if ( !isNaN(index) ) {
+      if ( (index < 0) || (index >= taskList.length) ) { $("#ERROR-MSG").append("<li>Error: Selected index is out of bounds.</li>"); }
+      else {
+        if ( validText(name) ) { taskList[index].name = name; }
+        else { $("#ERROR-MSG").append("<li>Error: *NAME* field was either left empty or invalid</li>"); }
+
+        if ( validText(deadLine) ) { taskList[index].deadline = deadLine; }
+        else { $("#ERROR-MSG").append("<li>Error: *DEADLINE* field was either left empty or invalid</li>"); }
+      }
+    }
+    else {
+      $("#ERROR-MSG").append("<li>Error: Invalid index given. Must be a number.</li>");
+    }
+
+    flashErrors();
+    listTasks();
+
+  });
+  //DELETE
+  $("button[name=removeButton]").click(function(){
+    $("input[name='delete-index']").val("0");
+    $("#INPUT-removeTask").toggle();
+    $("#OPTIONS").toggle();
+  });
+  $("#INPUT-removeTask>form").submit(function(event){
+    event.preventDefault();
+    var index = parseInt( $("input[name='delete-index']").val() );
+
+    if ( (typeof index!="number") ) { $("#ERROR-MSG").append("<li>Error: Invalid index given. Must be a number.</li>"); }
+    else if ( (index >= 0) && (index < taskList.length) ) { $("#ERROR-MSG").append("<li>Error: Selected index is out of bounds.</li>"); }
+    else { taskList.splice(index,1); }
+
+    console.log( $("#ERROR-MSG").children() );
+    console.log( taskList );
+
+
+    flashErrors();
+    listTasks();
+  });
+  //CANCEL FORM
+  $(".cancel").click(function(event){
+    event.preventDefault();
+    $("#INPUT-newTask, #INPUT-editTask, #INPUT-removeTask").hide();
+    $("#INPUT-newTask>input, #INPUT-editTask>input, #INPUT-removeTask>input").val("");
+    $("div#OPTIONS").show();
+  });
+});
